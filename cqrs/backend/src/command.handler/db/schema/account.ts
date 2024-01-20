43b46ebdb4706965cdcd8d8ea90a466acfd12041
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-01-20 18:41:50 -0300

    refactor(cqrs): add not null in some db fields

--- cqrs/backend/src/command.handler/db/schema/account.ts
+++ cqrs/backend/src/command.handler/db/schema/account.ts
@@ -1,14 +1,15 @@
+import { AccountType } from "@command.handler/models/Account";
 import { sql } from "drizzle-orm";
 import { decimal, mysqlTable, timestamp, mysqlEnum, int, varchar } from "drizzle-orm/mysql-core";
 
 export const account = mysqlTable('account', {
   id: int("id").primaryKey().autoincrement(),  
-  type: mysqlEnum("type", ['checking', 'savings', 'investment']).notNull(),
+  type: mysqlEnum("type", [AccountType.checking, AccountType.investment, AccountType.savings]).notNull(),
   wallet: varchar('wallet', { length: 16 }).unique().notNull(),
   balance: decimal("balance", {
     precision: 15,
     scale: 2,
-  }).default('0.00'),
+  }).default('0.00').notNull(),
   opened_at: timestamp("opened_at").notNull().default(sql`CURRENT_TIMESTAMP`),
   updated_at: timestamp("updated_at").notNull().default(sql`CURRENT_TIMESTAMP`),
 });
--- cqrs/backend/src/command.handler/db/schema/transaction.ts
+++ cqrs/backend/src/command.handler/db/schema/transaction.ts
@@ -1,15 +1,16 @@
 import { sql } from "drizzle-orm";
 import { int, decimal, mysqlTable, timestamp, mysqlEnum, varchar } from "drizzle-orm/mysql-core";
 import { account } from "./account";
+import { TransactionType } from "@command.handler/models/Transaction";
 
 export const transaction = mysqlTable('transaction', {
   id: int("id").primaryKey().autoincrement(),
   accountId: int("accountId").notNull().references(() => account.id),
-  type: mysqlEnum("type", ['deposit', 'withdrawal', 'transfer']).notNull(),
+  type: mysqlEnum("type", [TransactionType.deposit, TransactionType.withdrawal, TransactionType.transfer]).notNull(),
   currency: varchar('currency', { length: 10 }).notNull(),
   amount: decimal("balance", {
     precision: 15,
     scale: 2,
-  }).default('0.00'),
-  created_at: timestamp("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+  }).default('0.00').notNull(),
+  created_at: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
 });

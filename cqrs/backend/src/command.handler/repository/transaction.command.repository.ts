f7154704c5c7bfb883759da613136f9c47918739
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-01-20 18:41:04 -0300

    refactor(cqrs): add list method to transaction command repo

--- cqrs/backend/src/command.handler/repository/transaction.command.repository.ts
+++ cqrs/backend/src/command.handler/repository/transaction.command.repository.ts
@@ -1,7 +1,9 @@
 import { transaction } from "@command.handler/db/schema/transaction";
+import { Transaction } from "@command.handler/models/Transaction";
 import { MySqlInsertValue } from "drizzle-orm/mysql-core";
 import { MySqlRawQueryResult } from "drizzle-orm/mysql2";
 
 export interface TransactionCommandRepository {
   save(values: MySqlInsertValue<typeof transaction>): Promise<MySqlRawQueryResult | undefined>;
+  list(accountId: number): Promise<Array<Transaction>>;
 }
--- cqrs/backend/src/command.handler/repository/transaction.command.ts
+++ cqrs/backend/src/command.handler/repository/transaction.command.ts
@@ -2,11 +2,26 @@ import { MySqlInsertValue } from "drizzle-orm/mysql-core";
 import { db } from "@command.handler/db";
 import { TransactionCommandRepository } from "./transaction.command.repository";
 import { transaction } from "@command.handler/db/schema/transaction";
+import { eq } from "drizzle-orm";
+import { Transaction } from "@command.handler/models/Transaction";
 
 export class TransactionCommand implements TransactionCommandRepository {
   private readonly db = db;
 
   public async save(values: MySqlInsertValue<typeof transaction>) {
     return this.db.insert(transaction).values(values);
+  }
+
+  public async list(accountId: number): Promise<Array<Transaction>> {
+    return this.db.select({
+      id: transaction.id,
+      accountId: transaction.accountId,
+      currency: transaction.currency,
+      amount: transaction.amount,
+      type: transaction.type,
+      created_at: transaction.created_at,
+    }).from(transaction).where(
+      eq(transaction.accountId, accountId)
+    )
   }
 }

30a41c207f3a6523f1f952bf5e627165f5d63109
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-01-25 09:41:16 -0300

    feat: tailwindcss config

---
+++ cqrs/frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray-light': "#f9f9f9"
+      }
+    },
+  },
+  plugins: [],
+}
+

7b5ae829cb366da64588a1636484f06ec35f0869
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-01-25 12:51:12 -0300

    aside

--- cqrs/frontend/src/features/aside/item.tsx
+++ cqrs/frontend/src/features/aside/item.tsx
@@ -1,15 +1,15 @@
-import { children } from "solid-js";
 import { JSX } from "solid-js/jsx-runtime"
 
 interface ItemProps {
   Icon: JSX.Element;
-  text: string
+  text: string;
+  href: string;
 }
 
 export const Item = (props: ItemProps) => {
   return (
     <li>
-      <a class='flex items-center gap-3 p-2 rounded-md hover:transition-all hover:bg-slate-200 hover:bg-opacity-40 text-gray-600' href='#'>
+      <a class='flex items-center gap-3 p-2 rounded-md hover:transition-all hover:bg-slate-200 hover:bg-opacity-40 text-gray-600' href={props.href}>
         {props.Icon}
         <p class="text-sm font-semibold">{props.text}</p>
       </a>

234d9699631d4d0fe03e7b89a41cb0076f44af7f
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-01-27 13:54:49 -0300

    feat: button to refeatch balance

--- cqrs/frontend/src/features/aside/account.section.tsx
+++ cqrs/frontend/src/features/aside/account.section.tsx
@@ -1,6 +1,7 @@
 import { createResource, createSignal } from "solid-js";
 import { getAccount } from "../../services/account";
 import { useWalletContext } from "../../contexts/wallet.context";
+import { VsDebugRestart } from "solid-icons/vs";
 
 function formatBalance(balance: number) {
   return balance.toFixed(2);
@@ -8,7 +9,9 @@ function formatBalance(balance: number) {
 
 export const AccountSection = () => {
   const { state } = useWalletContext();
-  const [account] = createResource<Account>(() => getAccount(state.wallet));
+  const [account, { refetch }] = createResource<Account>(() =>
+    getAccount(state.wallet),
+  );
 
   return (
     <section class="sticky top-full grid gap-2 px-6 py-4">
@@ -19,11 +22,17 @@ export const AccountSection = () => {
         </strong>
       </p>
 
-      <p class="flex items-end gap-1 text-sm font-semibold">
+      <p class="flex items-center gap-1 text-sm font-semibold">
         Balance:
         <strong class="text-base leading-5 text-gray-600">
           {formatBalance(Number(account()?.balance || 0))}
         </strong>
+        <button
+          class="ml-auto rounded-full p-1 hover:bg-gray-400 hover:bg-opacity-40 hover:transition-all"
+          onClick={refetch}
+        >
+          <VsDebugRestart size={20} />
+        </button>
       </p>
     </section>
   );
